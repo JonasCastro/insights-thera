6cc60cb07ee4d7c3f04b046fc534a0a2fb6bc844
Jonas Castro <jonascastro128@gmail.com>
2021-12-22 20:21:42 -0300

    refactor: cria componente Search

---
+++ front/src/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+ <div class="wrapper d-flex justify-center" >
+    <v-text-field
+      class="search-input"
+      solo
+      label="Pesquise por termos ou categorias…"
+      append-icon="mdi-magnify"
+      hide-details
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
+
+<style>
+  .wrapper {
+    width:100%;
+    padding: 16px;
+  }
+  .search-input {
+    max-width: 382px !important;
+  }
+
+</style>
